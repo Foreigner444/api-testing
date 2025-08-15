abac3aad6ca5f3ef8ded6434eeb1818ed9f70d98
Foreigner444 <testingu259@gmail.com>
2025-08-15 10:29:01 +0300

    Fix environtmen

--- fixtures/allure.py
+++ fixtures/allure.py
@@ -4,5 +4,6 @@ from tools.allure.environment import create_allure_environment_file
 
 @pytest.fixture(scope="session", autouse=True)
 def save_allure_environment_file():
-    create_allure_environment_file()
-    yield
+    # create_allure_environment_file()
+    # yield
+    pass
